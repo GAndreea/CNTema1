476382ee5a3f21c496ac5f77779f47c161ac3340
Unknown <larisa.andrici@gmail.com>
2018-05-09 21:42:11 +0300

    tema4

--- main.py
+++ main.py
@@ -1,190 +1,221 @@
-from flask import Flask
-from flask import request
-from flask import render_template
+import functools
+import scipy.sparse
 import numpy as np
-import math
-import random
-import time
-import sys
-app = Flask(__name__)
-
-def ex_1():
-    i=1;
-    while True:
-        if (1+pow(10, -i)==1):
-            break;
-        x=i;
-        i = i + 1;
-    return -x-1;
-
-def ex_2():
-    x=1.0;
-    u=pow(10, ex_1());
-    y=u;
-    z=u;
-    return (((x+y)+z)==(x+(y+z)))
-
-def ex_2_b():
-    x=1.0;
-    y=0.4;
-    z=1.2;
-    while ((x*y)*z==x*(y*z)):
-        y += 0.01
-        z += 0.1
-    print (x,y,z)
 
 
-#inmultire normala
-def multipl(X,Y):
-    result = [[0 for x in range(len(X))] for y in range(len(Y))]
-    for i in range(len(X)):
-        # iterate through columns of Y
-        for j in range(len(Y)):
-            # iterate through rows of Y
-            for k in range(len(Y)):
-                result[i][j] += X[i][k] * Y[k][j]
-    return result;
+# x_stare_urmatoare(i) = (vectorulB(i) - suma(linia i din matrice * x_din stare_precendenta)(j)) / linia i din matrice(i)
 
-#inmultire strassen - matrici 2*2
-def inmultire(M1, M2):
-    w = 2;
-    M3 = [[0 for x in range(w)] for y in range(w)]
+def sparse_matrix(fisier):
+    f = open(fisier, "r")
+    n = int(f.readline());
+    matrix = [[]] * n;
+    lines = f.readlines()[n + 2:];
+    nr = 0;
+    for line in lines:
+        vals = line.split(',');
+        # vals[0]-valoarea vals[1]-linia vals[2]-coloana
+        m = list(matrix[int(vals[1])]);
+        ok = 0;
+        for t in range(len(m)):
+            if m[t][1] == vals[2]:
+                x = float(m[t][0]) + float(vals[0])
+                new_t = (float(x), int(m[t][1]))
+                m[t] = new_t;
+                ok = 1;
+        matrix[int(vals[1])] = list(m);
+        if (ok == 0):
+            t = (float(vals[0]), int(vals[2]));
+            new_line = list(matrix[int(vals[1])])
+            new_line.append(t)
+            matrix[int(vals[1])] = list(new_line);
+            # matrix[int(vals[1])].append(t);
+            nr += 1;
+        mn = sorted(matrix[int(vals[1])], key=functools.cmp_to_key(compare));
+        matrix[int(vals[1])] = list(mn);
+    return matrix
 
-    p1 = (M1[0][0] + M1[1][1])*(M2[0][0]+M2[1][1])
-    p2 = (M1[1][0]+M1[1][1])*M2[0][0]
-    p3 = M1[0][0]*(M2[0][1]-M2[1][1])
-    p4 = M1[1][1]*(M2[1][0]-M2[0][0])
-    p5=(M1[0][0]+M1[0][1])*M2[1][1]
-    p6=(M1[1][0]-M1[0][0])*(M2[0][0]+M2[0][1])
-    p7=(M1[0][1]-M1[1][1])*(M2[1][0]+M2[1][1])
 
-    M3[0][0] = p1+p4-p5+p7;
-    M3[0][1] = p3+p5;
-    M3[1][0] = p2+p4
-    M3[1][1] = p1+p3-p2+p6;
+def dot(v1, v2):
+    s = 0
+    k = 0
+    p = 0
+    while (k < len(v1) and p < len(v2)):
+        if (v1[k][1] == v2[p][1]):
+            s += v1[k][0] * v2[p][0]
+            k += 1
+            p += 1
+        else:
+            if (v1[k][1] > v2[p][1]):
+                p += 1
+            else:
+                k += 1
+    return s
 
-    return M3;
 
-def suma(M1, M2):
-    M3 = [[0 for x in range(len(M1))] for y in range(len(M1))]
-    for i in range (0,len(M1)):
-        for j in range (0,len(M1)):
-            M3[i][j] = M1[i][j] + M2[i][j];
-    return M3
+def transpune(m1):
+    matrix = [[]] * len(m1);
+    for i in range(len(m1)):
+        for t in m1[i]:
+            ctuplu = (t[0], i);
+            new_line = list(matrix[int(t[1])])
+            new_line.append(ctuplu)
+            matrix[int(t[1])] = list(new_line);
+    return matrix;
 
-def dif(M1, M2):
-    M3 = [[0 for x in range(len(M1))] for y in range(len(M1))]
-    for i in range (0,len(M1)):
-        for j in range (0,len(M1)):
-            M3[i][j] = M1[i][j] - M2[i][j];
-    return M3
 
-#redimensioneaza matricea la o dimensiune n
-def redim(M,n):
-    R = [[0 for x in range(n)] for y in range(n)]
-    m = min(len(M),n)
-    for i in range(m):
-        for j in range(m):
-            R[i][j] = M[i][j]
-    return R;
+def multiply(m1, m2):
+    matrix = [[]] * len(m1)
+    for i in range(len(m1)):
+        for j in range(len(m2)):
+            x = dot(m1[i], m2[j])
+            if (x != 0):
+                t = (x, j)
+                new_line = list(matrix[i])
+                new_line.append(t)
+                matrix[i] = list(new_line);
+    return matrix;
 
-#generare matrice random de dim n
-def gen_mat(n):
-    R = [[0 for x in range(n)] for y in range(n)]
+
+def diferenta(m1, m2):
+    matrix = [[]] * len(m1);
+    for i in range(len(m2)):
+        k = 0
+        l = 0
+
+        while (k < len(m1[i]) and l < len(m2[i])):
+            if (m1[i][k][1] == m2[i][l][1]):
+                x = float(m1[i][k][0]) - float(m2[i][l][0]);
+                t = (x, m1[i][k][1]);
+                new_line = list(matrix[i])
+                new_line.append(t)
+                matrix[i] = list(new_line);
+                k += 1;
+                l += 1;
+            else:
+                if (int(m1[i][k][1]) < int(m2[i][l][1])):
+                    # matrix[i].append(m1[i][k]);
+                    new_line = list(matrix[i])
+                    new_line.append(m1[i][k])
+                    matrix[i] = list(new_line);
+                    k += 1;
+                else:
+                    new_line = list(matrix[i])
+                    new_line.append(m2[i][l])
+                    matrix[i] = list(new_line);
+                    # matrix[i].append(m2[i][l])
+                    l += 1;
+        # print(k,l)
+        while (k < len(m1[i])):
+            new_line = list(matrix[i])
+            new_line.append(m1[i][k])
+            matrix[i] = list(new_line);
+            k += 1;
+        while (l < len(m2[i])):
+            new_line = list(matrix[i])
+            new_line.append(m2[i][l])
+            matrix[i] = list(new_line);
+            l += 1;
+    return matrix;
+
+
+def compare(t1, t2):
+    return (int(t1[1]) > int(t2[1])) - (int(t1[1]) < int(t2[1]))
+
+
+def get_b(fisier):
+    f = open(fisier, "r")
+    n = int(f.readline());
+    matrix = [[]] * n
+    lines = f.readlines()[1:n + 1]
+    res = []
+    for i in range(len(lines)):
+        t = (float(lines[i]), 0)
+        new_line = list(matrix[i])
+        new_line.append(t)
+        matrix[i] = list(new_line);
+    return matrix
+
+
+def checkDiagonal(matrix, n):
+    hasValue = True
     for i in range(n):
-        for j in range(n):
-            R[i][j] =  random.randint(0, 30)
-    return R;
-
-one_time_only = 1;
-
-#algoritm Strassen
-def algo(M1, M2, d):
-    temp = math.log(len(M1),2)
-    temp = int(temp)
-    if (pow(2, temp)!=len(M1)):
-        temp = temp + 1;
-        new_n = pow(2,temp);
-        M1 = redim(M1, new_n);
-        M2 = redim(M2, new_n);
-    n = math.log(len(M1), 2) - 1
-    if (n+1<=d):
-        return multipl(M1, M2)
-    if (n==0):
-        return inmultire(M1,M2);
+        line = [x[1] for x in matrix[i]]
+        if i not in line:
+            hasValue = False
+    if (hasValue == True):
+        return True
     else:
-        n = math.log(len(M1), 2) - 1
-        n=int(n)
-        n=pow(2,n)
-        m = math.log(len(M2), 2) - 1
-        m = int(m)
-        m = pow(2, m)
-        A = np.array(M1)[0:n,0:n]
-        B = np.array(M1)[0:n, n:len(M1)]
-        C = np.array(M1)[n:len(M1), 0:n]
-        D = np.array(M1) [n:len(M1), n:len(M1)]
-        E = np.array(M2)[0:m, 0:m]
-        F = np.array(M2)[0:m, m:len(M2)]
-        G = np.array(M2)[m:len(M2), 0:m]
-        H = np.array(M2)[m:len(M2), m:len(M2)]
+        return False
 
-        p1 = algo(suma(A, D), suma(E, H), d)
-        p2 = algo(suma(C, D), E,  d)
-        p3 = algo(A, dif(F, H), d)
-        p4 = algo(D, dif(G, E),  d)
-        p5 = algo(suma(A, B), H,  d)
-        p6 = algo(dif(C, A), suma(E, F), d)
-        p7 = algo(dif(B, D), suma(G, H), d)
 
-        if (1==1):
-            X = suma(dif(suma(p1 , p4), p5) , p7);
-            Y = suma(p3 , p5);
-            Z = suma(p2 , p4);
-            W = suma(dif(suma(p1 , p3), p2) , p6);
-            R = [[0 for x in range(len(X)+len(Y))] for y in range(len(X)+len(Y))]
-            for i in range (len(X)):
-                for j in range (len(X)):
-                    R[i][j]=X[i][j]
-            for i in range (len(Y)):
-                for j in range (len(Y)):
-                    R[i][j+len(Y)]=Y[i][j]
-            for i in range (len(Z)):
-                for j in range (len(Z)):
-                    R[i+len(Z)][j] = Z[i][j]
-            for i in range (len(W)):
-                for j in range (len(W)):
-                    R[i+len(W)][j+len(W)] = W[i][j]
-            return R;
+def nextValues(matrix, vectorB, xValues, n):
+    for i in range(n):
+        line = matrix[i]
+        bi = vectorB[i]
+        divisor = 1
+        for j in range(len(line)):
+            if (line[j][1] != i):
+                bi = bi - line[j][0] * xValues[j]
+            if (line[j][1] == i):
+                divisor = line[j][0]
+        bi = bi / divisor
+        xValues[i] = bi
+    return xValues
 
-res1 = ex_1()
-res2 = "(((x+y)+z)==(x+(y+z))): " + str(ex_2())
 
-@app.route('/')
-def my_form():
-    global res1
-    global res2
-    return render_template("Tema1.html", res1 = res1, res2=res2, M1=[[]], M2=[[]], R=[[]])
+def checkFinalState(xValues, newxValues, precision):
+    ok = True
+    for i in range(len(xValues)):
+        if (abs(xValues[i] - newxValues[i]) >= precision):
+            ok = False
+    return ok
 
-app.debug=True
-@app.route('/', methods=['POST'])
-def my_form_post():
-    global res1
-    global res2
-    d = request.form['dValue']
-    n = request.form['nValue']
-    d = int(d)
-    n= int(n)
-    M1 = gen_mat(n)
-    M2 = gen_mat(n)
-    start_time2 = time.time()
-    R2 = multipl(M1, M2)
-    print(time.time() - start_time2)
-    start_time = time.time()
-    R = algo(M1, M2, d)
-    print(time.time()-start_time)
 
-    R=redim(R,n)
-    return render_template("Tema1.html", res1=res1,res2=res2, M1=M1, M2=M2, R=R)
+def norma(matrix, xValues, vectorB):
+    print(xValues)
+    m = multiply(matrix, transpune(xValues))
+    #bAsMatrix = [[tuple([x, 0])] for x in vectorB]
+    print(m)
+    print(vectorB)
+    norma = diferenta(m, vectorB)
+    max = norma[0][0][0]
+    for line in norma:
+        for column in line:
+            if abs(column[0]) > max:
+                max = column[0]
+    return max
 
-if __name__ == '__main__':
-    app.run()
+
+def main():
+    f = open("m_rar_2018_4.txt", "r")
+    n = int(f.readline())
+    v = get_b("m_rar_2018_4.txt")
+    precision = 10 ** -7
+    vectorB = [x[0][0] for x in v]
+    matrix = sparse_matrix("m_rar_2018_4.txt")
+    xValues = [0] * (n)
+    finished = False
+    nrOfIteration = 0
+    if (checkDiagonal(matrix, n) == True):
+        while finished == False and nrOfIteration < 10000:
+            prevIteration = xValues[:]
+            newxValues = nextValues(matrix, vectorB, xValues, n)
+            # asta trebuie afisata in html
+            print("xvalues la iteratia ", nrOfIteration, " : ", xValues)
+            finished = checkFinalState(prevIteration, newxValues, precision)
+            nrOfIteration = nrOfIteration + 1
+            print("nrOfIteration", nrOfIteration)
+    else:
+        print("Diagonala are valori nule")
+        return
+    xValuesTransformed = [[tuple([x, 0])] for x in xValues]
+
+    bAsMatrix = list(map(lambda x: [(x, 0)], vectorB))
+
+    norma2 = norma(matrix, xValuesTransformed, bAsMatrix)
+    # asta trebuie afisata in html
+    print(norma2)
+
+
+if __name__ == "__main__":
+    main()
